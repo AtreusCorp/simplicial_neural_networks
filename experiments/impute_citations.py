7ee4a3c93f98c219af1c7a7c7fcf9f0bb58464d4
AtreusCorp <keirn.m@gmail.com>
2021-02-19 12:27:18 -0500

    Pull out hparams and implement resnet style input addition

--- experiments/impute_citations.py
+++ experiments/impute_citations.py
@@ -7,18 +7,18 @@ import torch.utils.data as data
 import numpy as np
 import sys
 
-import sys
 sys.path.append('.')
 import scnn.scnn
 import scnn.chebyshev
 
 
 class MySCNN(nn.Module):
-    def __init__(self, colors = 1):
+    def __init__(self, colors = 1, res_add = False):
         super().__init__()
 
         assert(colors > 0)
         self.colors = colors
+        self.res_add = res_add
 
         num_filters = 30
         variance = 0.01
@@ -37,7 +37,6 @@ class MySCNN(nn.Module):
         self.C2_1 = scnn.scnn.SimplicialConvolution(5, self.colors, num_filters * self.colors, variance=variance)
         self.C2_2 = scnn.scnn.SimplicialConvolution(5, num_filters * self.colors, num_filters * self.colors, variance=variance)
         self.C2_3 = scnn.scnn.SimplicialConvolution(5, num_filters * self.colors, self.colors, variance=variance)
-
 
 
     def forward(self, Ls, Ds, adDs, xs):
@@ -71,17 +70,21 @@ class MySCNN(nn.Module):
         out1_3 = self.C1_3(Ls[1], nn.LeakyReLU()(out1_2)) #+ self.D01_3(nn.LeakyReLU()(out0_2)) + self.D21_2(nn.LeakyReLU()(out2_2))
         out2_3 = self.C2_3(Ls[2], nn.LeakyReLU()(out2_2)) #+ self.D12_3(nn.LeakyReLU()(out1_2))
 
-        #return [out0_3, torch.zeros_like(xs[1]), torch.zeros_like(xs[2])]
-        #return [torch.zeros_like(xs[0]), out1_3, torch.zeros_like(xs[2])]
+        if self.res_add:
+            return[
+                out0_3 + 0.5 * xs[0],
+                out1_3 + 0.5 * xs[1],
+                out2_3 + 0.5 * xs[2],
+            ]
         return [out0_3, out1_3, out2_3]
 
-def load_cochains(path, batch_size = 1, topdim = 2):
+def load_cochains(path, batch_size = 1, top_dim = 2):
     """ Loads the cochains contained at path.
 
     Args:
         path: the string of the path to load
         batch_size: the batch size to load in with
-        topdim: the top dimension of simplices to consider
+        top_dim: the top dimension of simplices to consider
 
     Returns:
         A list containing elements consisting of batch_size cochains contained
@@ -91,7 +94,7 @@ def load_cochains(path, batch_size = 1, topdim = 2):
     signal = np.load(path, allow_pickle=True)
     raw_data=[list(signal[i].values()) for i in range(len(signal))]
 
-    for d in range(topdim + 1):
+    for d in range(top_dim + 1):
         cochain_target = torch.zeros((batch_size, 1, len(raw_data[d])),
             dtype=torch.float, requires_grad = False)
 
@@ -101,14 +104,14 @@ def load_cochains(path, batch_size = 1, topdim = 2):
         cochains.append(cochain_target)
     return cochains
 
-def construct_model_inputs(path_prefix, file_identifier, topdim = 2,
+def construct_model_inputs(path_prefix, file_identifier, top_dim = 2,
         batch_size = 1, percentage_missing_values = 30):
     """ Returns the cochain inputs, targets, Laplacians, Ls, Ds, and adDs.
 
     Args:
         path_prefix: the directory to look under
         file_identifier: the unique file identifier for the data
-        topdim: the top dimension of simplices to consider
+        top_dim: the top dimension of simplices to consider
 
     Returns:
         A tuple consisting of the following matrices: Laplacians, Ls, Ds, adDs
@@ -117,11 +120,11 @@ def construct_model_inputs(path_prefix, file_identifier, topdim = 2,
         path=f'{path_prefix}/{file_identifier}_'
              f'percentage_{percentage_missing_values}_input_damaged.npy',
         batch_size=batch_size,
-        topdim=topdim)
+        top_dim=top_dim)
     cochain_target = load_cochains(
         path=f'{path_prefix}/{file_identifier}_cochains.npy',
         batch_size=batch_size,
-        topdim=topdim)
+        top_dim=top_dim)
     laplacians = np.load(
         '{}/{}_laplacians.npy'.format(
             path_prefix, file_identifier), allow_pickle=True)
@@ -132,19 +135,38 @@ def construct_model_inputs(path_prefix, file_identifier, topdim = 2,
             scnn.scnn.coo2tensor(
                 scnn.chebyshev.normalize(
                     laplacians[i],half_interval=True))
-            for i in range(topdim + 1)
+            for i in range(top_dim + 1)
     ]
     Ds = [
             scnn.scnn.coo2tensor(
                 boundaries[i].transpose()) 
-            for i in range(topdim + 1)
+            for i in range(top_dim + 1)
     ]
     adDs = [
-        scnn.scnn.coo2tensor(boundaries[i]) for i in range(topdim + 1)
+        scnn.scnn.coo2tensor(boundaries[i]) for i in range(top_dim + 1)
     ]
     return cochain_input, cochain_target, laplacians, Ls, Ds, adDs
 
-def main():
+class Hparams():
+    def __init__(
+            self,
+            learning_rate: float,
+            batch_size: int,
+            train_steps: int,
+            top_dim: int,
+            optimizer: torch.optim.Optimizer,
+            loss_criterion: torch.nn.Module,
+            res_add: bool):
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.train_steps = train_steps
+        self.top_dim = top_dim
+        self.optimizer = optimizer
+        self.loss_criterion = loss_criterion
+        self.res_add = res_add
+
+
+def train_and_test(hparams: Hparams):
     torch.manual_seed(1337)
     np.random.seed(1337)
 
@@ -153,20 +175,18 @@ def main():
     file_identifier=sys.argv[3]
     percentage_missing_values=sys.argv[4]
     cuda = False
-    learning_rate = 0.001
-    batch_size = 1
-    train_steps = 150
-    topdim = 2
+    batch_size = hparams.batch_size
+    top_dim = hparams.top_dim
+    network = MySCNN(colors=1, res_add=hparams.res_add)
+    optimizer = hparams.optimizer(
+        network.parameters(), lr=hparams.learning_rate)
+    criterion = hparams.loss_criterion
 
     cochain_train_input, cochain_train_target,\
         laplacians, Ls, Ds, adDs = construct_model_inputs(
             path_prefix=path_prefix, file_identifier=file_identifier,
-            topdim=topdim, batch_size=batch_size,
+            top_dim=top_dim, batch_size=batch_size,
             percentage_missing_values=percentage_missing_values)
-    network = MySCNN(colors = 1)
-    optimizer = torch.optim.Adam(network.parameters(), lr=learning_rate)
-    criterion = nn.MSELoss(reduction="sum")
-
     num_params = 0
     print("Parameter counts:")
     for param in network.parameters():
@@ -186,10 +206,10 @@ def main():
     print(
         [
             len(masks[d]) / len(cochain_train_target[d][0,0,:])
-                for d in range(topdim + 1)
+                for d in range(top_dim + 1)
         ])
 
-    for i in range(train_steps):
+    for i in range(hparams.train_steps):
         xs = [cochain_input.clone(
             ) for cochain_input in cochain_train_input]
 
@@ -198,19 +218,19 @@ def main():
 
         loss = torch.FloatTensor([0.0])
         for b in range(batch_size):
-            for d in range(topdim + 1):
+            for d in range(top_dim + 1):
                 loss += criterion(ys[d][
                     b, 0, masks[d]], cochain_train_target[d][
                         b, 0, masks[d]])
 
-        detached_ys = [ys[d].detach() for d in range(0, topdim+1)]
+        detached_ys = [ys[d].detach() for d in range(0, top_dim+1)]
 
         if np.mod(i, 10) == 0:
-            for d in range(0,topdim+1):
+            for d in range(0,top_dim+1):
                 np.savetxt(
                     f'{logdir}/output_{i}_{d}.txt', detached_ys[d][0,0,:])
 
-        for d in range(0, topdim+1):
+        for d in range(0, top_dim+1):
             predictionlogf = open(f'{logdir}/prediction_{i}_{d}.txt', "w")
             actuallogf = open(f'{logdir}/actual_{i}_{d}.txt', "w")
 
@@ -234,13 +254,12 @@ def main():
     masks = [
         list(set(range(
             cochain_train_input[i].shape[-1])) - set(masks_all_deg[i].values()))
-        for i in range(topdim + 1)
+        for i in range(top_dim + 1)
     ]
     test_criterion = nn.L1Loss(reduction="sum")
     loss = 0
     ys = network(Ls, Ds, adDs, cochain_train_input)
-    ys = [torch.round(tensor) for tensor in ys]
-    for d in range(topdim + 1):
+    for d in range(top_dim + 1):
         loss += test_criterion(ys[d][
             0, 0, masks[d]], cochain_train_target[d][0, 0, masks[d]])
     print(f'Train set loss: {loss}')
@@ -250,7 +269,7 @@ def main():
         laplacians, Ls, Ds, adDs = construct_model_inputs(
             path_prefix=path_prefix,
             file_identifier='test_' + file_identifier,
-            topdim=topdim,
+            top_dim=top_dim,
             batch_size=batch_size,
             percentage_missing_values=percentage_missing_values)
 
@@ -260,16 +279,34 @@ def main():
     masks = [
         list(set(range(
             cochain_test_input[i].shape[-1])) - set(test_masks[i].values()))
-        for i in range(topdim + 1)
+        for i in range(top_dim + 1)
     ]
     test_criterion = nn.L1Loss(reduction="sum")
     loss = 0
     ys = network(Ls, Ds, adDs, cochain_test_input)
-    ys = [torch.round(tensor) for tensor in ys]
-    for d in range(topdim + 1):
+    for d in range(top_dim + 1):
         loss += test_criterion(ys[d][
             0, 0, masks[d]], cochain_test_target[d][0, 0, masks[d]])
     print(f'Test set loss: {loss}')
 
 if __name__ == "__main__":
-    main()
+    train_steps = int(sys.argv[5]) if len(sys.argv) > 4 else 30
+    atreus_hparams = Hparams(
+        learning_rate=0.01,
+        batch_size=1,
+        train_steps=train_steps,
+        top_dim=2,
+        optimizer=torch.optim.Adagrad,
+        loss_criterion=nn.MSELoss(reduction="sum"),
+        res_add=True,
+    )
+    original_hparams = Hparams(
+        learning_rate=0.001,
+        batch_size=1,
+        train_steps=train_steps,
+        top_dim=2,
+        optimizer=torch.optim.Adam,
+        loss_criterion=nn.L1Loss(reduction="sum"),
+        res_add=False,
+    )
+    train_and_test(original_hparams)
